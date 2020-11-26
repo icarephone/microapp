b9e5be44ea459c1a3478c94fc590c11942b2e16a
Sqeven <sqeven@qq.com>
2020-11-26 10:11:15 +0800

    ...

--- apis/auth/auth.go
+++ apis/auth/auth.go
@@ -1,4 +1,4 @@
-// Copyright 2020 FastWeGo
+// Copyright 2020 Icarephone
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
